34a32a4ab1cd4755b58f343cdee59e70b20bc380
Julian Molina <jumofe95@gmail.com>
2016-11-17 13:37:12 +0100

    step 1 form created

--- src/OrchardBundle/Controller/DefaultController.php
+++ src/OrchardBundle/Controller/DefaultController.php
@@ -5,39 +5,44 @@ namespace OrchardBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use OrchardBundle\Entity\Orchard;
-use OrchardBundle\Form\OrchardType;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('OrchardBundle:Default:index.html.twig');
-    }
-
-
-  public function newAction(Request $request)
+  public function indexAction()
   {
-    //build the form
-    $orchard = new Orchard();
-    $form = $this->createForm(OrchardType::class, $orchard);
-   
-   //handle the submit (will only happen on POST)
-    $form->handleRequest($request);
-    if ($form->isSubmitted() && $form->isValid()){
-      
-      //get the data
-      $event = $form->getData();
+    return $this->render('OrchardBundle:Default:new_orchard.html.twig');
+  }
 
-      //Uploads them to the DB
+  public function insertAction(Request $request) {
+    if ($request->isXMLHttpRequest()) {
+     
+      $id = $request->request->get('id');
+      $name = $request->request->get('name');
+      $town = $request->request->get('town');
+      $street = $request->request->get('street');
+      $number = $request->request->get('number');
+      $zipCode = $request->request->get('zipcode');
+
+      $orchard = $this->getDoctrine()->getRepository('OrchardBundle:Orchard')->findOneById($id);
+      if($orchard == null) {
+        $orchard = new Orchard();
+      }
+
+      $orchard->setName($name);
+      $orchard->setTown($town);
+      $orchard->setStreet($street);
+      $orchard->setNumber($number);
+      $orchard->setZipCode($zipCode);
+
       $em = $this->getDoctrine()->getManager();
-      $em->persist($event);
+      $em->persist($orchard);
       $em->flush();
 
-
-      return $this -> redirectToRoute('orchard_homepage');
+      $id = $orchard->getId();
+      return new JsonResponse(array('id' => $id));
+    } else {
+          // Do something else
     }
-
-
-    return $this->render('OrchardBundle:Default:new_orchard.html.twig', array("form" => $form->createView() ));
   }
 }
