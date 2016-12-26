192df0b66f9a3e49214be0ebfe6cc8888d7c2b38
pablo <pablomaganapablomagub@gmail.com>
2016-12-26 19:59:55 +0100

    images.js modificado

--- web/js/images.js
+++ web/js/images.js
@@ -1,15 +1,3 @@
-// document.getElementById('fileinput').addEventListener('change', function(){
-//     for(var i = 0; i<this.files.length; i++){
-//         var file =  this.files[i];
-//         // This code is only for demo ...
-//         console.group("File "+i);
-//         console.log("name : " + file.name);
-//         console.log("size : " + file.size);
-//         console.log("type : " + file.type);
-//         console.log("date : " + file.lastModified);
-//         console.groupEnd();
-//     }
-// }, false);
 //capturar imagenes subidas
 function previewImage(file) {
     var galleryId = "gallery";
@@ -20,23 +8,36 @@ function previewImage(file) {
     if (!file.type.match(imageType)) {
         throw "File Type must be an image";
     }
+    $.ajax({
+      type: "POST",
+      url:"/orchard/images/upload",
+      data: {
+        'name':image.title,
+        'src':image.src,
+        'description':'descripción vacia'
+      },
+      success: function(){createImageElement(file)}
+    });
 
-    var thumb = document.createElement("div");
-    thumb.classList.add('thumbnail'); // Add the class thumbnail to the created div
+}
 
-    var img = document.createElement("img");
-    var des = document.createElement("INPUT");
-    des.setAttribute("type","text");
-    des.classList.add("descripcion");
-    img.file = file;
-    thumb.appendChild(img);
-    thumb.appendChild(des);
-    gallery.appendChild(thumb);
+function createImageElement(file){
+  var thumb = document.createElement("div");
+  thumb.classList.add('thumbnail'); // Add the class thumbnail to the created div
 
-    // Using FileReader to display the image content
-    var reader = new FileReader();
-    reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-    reader.readAsDataURL(file);
+  var img = document.createElement("img");
+  var des = document.createElement("INPUT");
+  des.setAttribute("type","text");
+  des.classList.add("descripcion");
+  img.file = file;
+  thumb.appendChild(img);
+  thumb.appendChild(des);
+  gallery.appendChild(thumb);
+
+  // Using FileReader to display the image content
+  var reader = new FileReader();
+  reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+  reader.readAsDataURL(file);
 }
 var files = null;
 var uploadfiles = document.querySelector('#fileinput');
