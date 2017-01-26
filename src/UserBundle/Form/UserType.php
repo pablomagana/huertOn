adc88ce3ffc6ddd16d797b870e6910fb1b39fdaf
Julian Molina <jumofe95@gmail.com>
2017-01-26 13:05:08 +0100

    diseños login-registro

--- src/UserBundle/Form/UserType.php
+++ src/UserBundle/Form/UserType.php
@@ -21,14 +21,14 @@ class UserType extends BaseRegistrationFormType {
 
   public function buildForm(FormBuilderInterface $builder, array $options) {
     $builder
-        ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'))
-        ->add('username', null, array('label' => 'Nombre', 'translation_domain' => 'FOSUserBundle'))
-        ->add('apellidos', null, array('label' => 'Apellidos'))
+        ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => false, 'translation_domain' => 'FOSUserBundle', 'attr' => array('placeholder' => 'Dirección de correo electrónico')))
+        ->add('username', null, array('label' => false, 'translation_domain' => 'FOSUserBundle', 'attr' => array('placeholder' => 'Nombre')))
+        ->add('apellidos', null, array('label' => false, 'attr' => array('placeholder' => 'Apellidos')))
         ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
             'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
             'options' => array('translation_domain' => 'FOSUserBundle'),
-            'first_options' => array('label' => 'form.password'),
-            'second_options' => array('label' => 'form.password_confirmation'),
+            'first_options' => array('label' => false, 'attr' => array('placeholder' => 'Contraseña')),
+            'second_options' => array('label' => false, 'attr' => array('placeholder' => 'Vuelve a escribir tu contraseña')),
             'invalid_message' => 'fos_user.password.mismatch',
         ))
     ;
