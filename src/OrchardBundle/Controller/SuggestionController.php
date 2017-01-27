103c3be9901b4cc44691c4e727d90fbf034bef2f
Julian Molina <jumofe95@gmail.com>
2017-01-27 17:27:51 +0100

    https://trello.com/c/rDch5yu3

--- src/OrchardBundle/Controller/SuggestionController.php
+++ src/OrchardBundle/Controller/SuggestionController.php
@@ -17,78 +17,87 @@ class SuggestionController extends Controller
   {
 
     $message = \Swift_Message::newInstance()
-        ->setContentType("text/html")
-        ->setSubject('Nueva sugerencia para tipos de huerto')
-        ->setFrom('parcellesflorida@gmail.com')
-        ->setTo('ab95david@gmail.com')
-        ->setBody(
-            $this->renderView(
-                'OrchardBundle:Default:email.html.twig',
-                array('orchard_type' => $orchard_type)
-            )
+    ->setContentType("text/html")
+    ->setSubject('Nueva sugerencia para tipos de huerto')
+    ->setFrom('parcellesflorida@gmail.com')
+    ->setTo('jumofe95@gmail.com')
+    ->setBody(
+      $this->renderView(
+        'OrchardBundle:Default:email.html.twig',
+        array('orchard_type' => $orchard_type)
         )
-    ;
-    $this->get('mailer')->send($message);
+        )
+        ;
+        $this->get('mailer')->send($message);
 
-    return new Response();
-  }
+        return new Response();
+      }
 
-  public function suggestAction(Request $request, $orchard_type, $accept)
-  {
+      public function addTypeDescriptionAction($orchard_type, $accept)
+      {
 
-    $user = $this->get('security.token_storage')->getToken()->getUser();
+        $user = $this->get('security.token_storage')->getToken()->getUser();
 
-    $cookies = $request->cookies;
-
-    $orchard = null;
-
-    if($cookies->has('ID_ORCHARD')) {
-      //El huerto está creado así que recuperamos el objeto de BBDD
-      $orchard = $this->getDoctrine()->getRepository('OrchardBundle:Orchard')->findOneById($cookies->get('ID_ORCHARD'));
-    }
-
-    if($accept) {
-      //Guardar en BBDD y relacionar con orchard
-      //Enviar mail de éxito al usuario
-      $orchardType = new OrchardType();
-      $orchardType->setName($orchard_type);
-      $orchard->addOrchardType($orchardType);
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($orchard);
-      $em->persist($orchardType);
-      $em->flush();
-
-      $message = \Swift_Message::newInstance()
-          ->setContentType("text/html")
-          ->setSubject('Se ha aceptado la sugerencia para tipos de huerto')
-          ->setFrom('parcellesflorida@gmail.com')
-          ->setTo($user->getEmail())
-          ->setBody(
-              $this->renderView(
-                  'OrchardBundle:Default:email.html.twig',
-                  array('orchard_type' => $orchard_type, 'accept' => $accept)
-              )
-          )
-      ;
-      $this->get('mailer')->send($message);
-
-    }else {
-      //Enviar mail de error al usuario
-      $message = \Swift_Message::newInstance()
+        if($accept == 'true') {
+          return $this->render('OrchardBundle:Default:orchard-type_add-description.html.twig', array('orchard_type' => $orchard_type));
+        }else {
+          //Enviar mail de error al usuario
+          $message = \Swift_Message::newInstance()
           ->setContentType("text/html")
           ->setSubject('No se ha aceptado la sugerencia para tipos de huerto')
           ->setFrom('parcellesflorida@gmail.com')
           ->setTo($user->getEmail())
           ->setBody(
-              $this->renderView(
-                  'OrchardBundle:Default:email.html.twig',
-                  array('orchard_type' => $orchard_type, 'accept' => $accept)
+            $this->renderView(
+              'OrchardBundle:Default:email.html.twig',
+              array('orchard_type' => $orchard_type, 'accept' => $accept)
               )
-          )
-      ;
-      $this->get('mailer')->send($message);
-    }
+              )
+              ;
+              $this->get('mailer')->send($message);
 
-    return new Response();
-  }
-}
+              return new Response();
+            }
+
+          }
+
+          public function suggestAction(Request $request, $orchard_type, $type_description) {
+            $user = $this->get('security.token_storage')->getToken()->getUser();
+
+            $cookies = $request->cookies;
+
+            $orchard = null;
+
+            if($cookies->has('ID_ORCHARD')) {
+              //El huerto está creado así que recuperamos el objeto de BBDD
+              $orchard = $this->getDoctrine()->getRepository('OrchardBundle:Orchard')->findOneById($cookies->get('ID_ORCHARD'));
+            }
+
+            //Guardar en BBDD y relacionar con orchard
+            //Enviar mail de éxito al usuario
+            $orchardType = new OrchardType();
+            $orchardType->setName($orchard_type);
+            $orchardType->setDescription($type_description);
+            $orchard->addOrchardType($orchardType);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($orchard);
+            $em->persist($orchardType);
+            $em->flush();
+
+            $message = \Swift_Message::newInstance()
+            ->setContentType("text/html")
+            ->setSubject('Se ha aceptado la sugerencia para tipos de huerto')
+            ->setFrom('parcellesflorida@gmail.com')
+            ->setTo($user->getEmail())
+            ->setBody(
+              $this->renderView(
+                'OrchardBundle:Default:email.html.twig',
+                array('orchard_type' => $orchard_type, 'accept' => 'true')
+                )
+                )
+                ;
+                $this->get('mailer')->send($message);
+
+                return new Response();
+              }
+            }
