998f477438b41dd5609eaceb5754a5b779280786
pablomagub <pablomagub@gmail.com>
2017-02-06 12:40:52 +0100

    metodo buscar huerto

--- src/HomeBundle/Controller/DefaultController.php
+++ src/HomeBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace HomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use OrchardBundle\Entity\Orchard;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,48 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('HomeBundle:Default:index.html.twig');
+    }
+
+    /**
+    * @Route("/find/{param}")
+    */
+    public function FindAction($param)
+    {
+      $repository = $this->getDoctrine()->getRepository('OrchardBundle:Orchard');
+
+      // createQueryBuilder() automatically selects FROM AppBundle:ORCHARD and aliases it to "o"
+      $query = $repository->createQueryBuilder('o')
+          ->where("o.town LIKE :param OR o.zipCode LIKE :param OR o.name LIKE :name")
+          ->setParameter('param', $param)
+          ->setParameter('name','%'.$param.'%')
+          //->orderBy('o.name', 'ASC')
+          ->getQuery();
+
+      $orchards = $query->getResult();
+      if (!$orchards) {
+        echo "vacio";
+      }
+      return $this->render('HomeBundle:Default:index.html.twig',array('orchards' => $orchards ));
+    }
+
+    //obtener distancia entre dos puntos(lat,long)
+    public function getDistance($latorchard, $longonrchard, $latUser, $longUser)
+    {
+        $earth_radius = 6371;
+        $dLat = deg2rad($latUser - $latContenedor);
+        $dLon = deg2rad($longUser - $longCont1);
+        $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($latContenedor)) * cos(deg2rad($latUser)) * sin($dLon / 2) * sin($dLon / 2);
+        $c = 2 * asin(sqrt($a));
+        $d = $earth_radius * $c;
+        return $d * 1000;
+    }
+
+    public function filterOrchard($coordenates){
+        ini_set('max_execution_time', 3000);
+        if(in_array($coordenates,$this->coordenatesContenedores)) {
+            return false;
+        }
+        array_push($this->coordenatesContenedores,$coordenates);
+        return true;
     }
 }
