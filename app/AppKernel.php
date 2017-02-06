8a452fd98dab5a93c40266c8e21d28d7c0492b8f
David Alòs Bosch <ab95david@gmail.com>
2017-02-06 13:09:53 +0100

    Add VichUploaderbundle, format routes and steps

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new OrchardBundle\OrchardBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
