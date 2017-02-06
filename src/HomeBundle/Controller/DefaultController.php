d6cc9a76866a2fd8c1ffbabf70169d51aa18d6ee
pablomagub <pablomagub@gmail.com>
2017-02-06 08:56:28 +0100

    homeBundle

---
+++ src/HomeBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace HomeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('HomeBundle:Default:index.html.twig');
+    }
+}
