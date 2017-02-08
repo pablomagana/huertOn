42d449f694927e8c4cf7405958d03f66f59262eb
Alex Torres <torres.erades.alex@gmail.com>
2017-02-08 20:01:05 +0100

    Finish findAction and send form from index.html.twig (user_latitude & user_longitude)

--- src/HomeBundle/Controller/DefaultController.php
+++ src/HomeBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace HomeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use OrchardBundle\Entity\Orchard;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -14,42 +15,57 @@ class DefaultController extends Controller
     return $this->render('HomeBundle:Default:index.html.twig');
   }
 
-  public function findAction($param, $user_latitude, $user_longitude)
+  public function findAction(Request $request)
   {
+    $param = $request->request->get('param');
+    $user_latitude = $request->request->get('user_latitude');
+    $user_longitude = $request->request->get('user_longitude');
+
     $repository = $this->getDoctrine()->getRepository('OrchardBundle:Orchard');
 
-    $query = $repository->createQueryBuilder('o')
-    ->addSelect(
-      'o.name, o.zipCode, o.town, ( 3959 * acos(cos(radians(' . $user_latitude . '))' .
-      '* cos( radians( o.latitude ) )' .
-      '* cos( radians( o.longitude )' .
-      '- radians(' . $user_longitude . ') )' .
-      '+ sin( radians(' . $user_latitude . ') )' .
-      '* sin( radians( o.latitude ) ) ) ) as distance')
-      ->where("o.town LIKE :param OR o.zipCode LIKE :param OR o.name LIKE :name")
-      ->setParameter('param','%'.$param.'%')
-      ->setParameter('name','%'.$param.'%')
-      ->orderBy('distance', 'ASC')
-      ->getQuery();
+    $query = null;
 
-      $orchards = $query->getResult();
+    if ($user_latitude != null || $user_longitude != null) {
+      $query = $repository->createQueryBuilder('o')
+      ->addSelect(
+        'o.name, o.zipCode, o.town, o.address, ( 3959 * acos(cos(radians(' . $user_latitude . '))' .
+        '* cos( radians( o.latitude ) )' .
+        '* cos( radians( o.longitude )' .
+        '- radians(' . $user_longitude . ') )' .
+        '+ sin( radians(' . $user_latitude . ') )' .
+        '* sin( radians( o.latitude ) ) ) ) as distance')
+        ->innerJoin('OrchardBundle:Image', 'i', 'WITH', 'i.orchard = o.id')
+        ->where("(o.town LIKE :param OR o.zipCode LIKE :param OR o.name LIKE :name) AND o.published = 1")
+        ->setParameter('param','%'.$param.'%')
+        ->setParameter('name','%'.$param.'%')
+        ->orderBy('distance', 'ASC')
+        ->getQuery();
+      }else {
+        $query = $repository->createQueryBuilder('o')
+        ->addSelect(
+          'o.name, o.zipCode, o.town, o.address')
+          ->innerJoin('OrchardBundle:Image', 'i', 'WITH', 'i.orchard = o.id')
+          ->where("(o.town LIKE :param OR o.zipCode LIKE :param OR o.name LIKE :name) AND o.published = 1")
+          ->setParameter('param','%'.$param.'%')
+          ->setParameter('name','%'.$param.'%')
+          ->orderBy('o.updatedAt', 'ASC')
+          ->getQuery();
+        }
 
-      if (!$orchards) {
-        echo "vacio";
+        $orchards = $query->getResult();
+
+        return $this->render('HomeBundle:Default:find.html.twig', array('orchards' => $orchards, 'param' => $param));
       }
 
-      return $this->render('HomeBundle:Default:index.html.twig', array('orchards' => $orchards));
-    }
+      public function showAction($id_orchard)
+      {
 
-    public function showAction($id_orchard)
-    {
+        $orchard = $this->container->get("orchard_service")->getOrchard($id_orchard);
 
-      $orchard = $this->container->get("orchard_service")->getOrchard($id_orchard);
-
-      return $this->render('HomeBundle:Default:orchard_profile.html.twig', array('orchard' => $orchard));
+        return $this->render('HomeBundle:Default:orchard_profile.html.twig', array('orchard' => $orchard));
+      }
+      public function searchAction()
+      {
+        return $this->render('HomeBundle:Default:search.html.twig');
+      }
     }
-    public function searchAction()
-    {
-      return $this->render('HomeBundle:Default:search.html.twig');
-    }
-  }
