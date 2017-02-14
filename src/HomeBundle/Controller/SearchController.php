f1cb275a2313c1efe8c4a9fd8bdb166fb5dca1eb
Julian Molina <jumofe95@gmail.com>
2017-02-14 10:50:39 +0100

    busqueda de huertos y eventos separado en dos paginas

--- src/HomeBundle/Controller/SearchController.php
+++ src/HomeBundle/Controller/SearchController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 class SearchController extends Controller
 {
 
-  public function findAction(Request $request)
+  public function findOrchardAction(Request $request)
   {
     $input_home_search = $request->request->get('input-home_search');
     $user_latitude = $request->request->get('user_latitude');
@@ -48,7 +48,51 @@ class SearchController extends Controller
 
     $orchards = $query->getResult();
 
-    return $this->render('HomeBundle:Default:find.html.twig', array('orchards' => $orchards, 'type' => $type));
+    return $this->render('HomeBundle:Default:find_orchard.html.twig', array('orchards' => $orchards, 'type' => $type));
   }
+
+
+
+  public function findEventAction(Request $request)
+  {
+    $input_home_search = $request->request->get('input-home_search');
+    $user_latitude = $request->request->get('user_latitude');
+    $user_longitude = $request->request->get('user_longitude');
+
+    $em = $this->getDoctrine()->getManager();
+
+    $repository = $this->getDoctrine()
+    ->getRepository('OrchardBundle:Orchard');
+
+    $query = null;
+    $type = null;
+
+    if($user_latitude != null || $user_longitude != null) {
+      $type = 'distance';
+      $query = $repository->createQueryBuilder('o')
+          ->addSelect('( 3959 * acos(cos(radians(' . $user_latitude . '))' .
+          '* cos( radians( o.latitude ) )' .
+          '* cos( radians( o.longitude )' .
+          '- radians(' . $user_longitude . ') )' .
+          '+ sin( radians(' . $user_latitude . ') )' .
+          '* sin( radians( o.latitude ) ) ) ) as distance')
+          ->where('o.name LIKE :param OR o.address LIKE :param')
+          ->setParameter('param', '%' . $input_home_search . '%')
+          ->addOrderBy('distance')
+          ->getQuery();
+    }else {
+      $type = 'updatedAt';
+      $query = $repository->createQueryBuilder('o')
+          ->where('o.name LIKE :param OR o.address LIKE :param')
+          ->setParameter('param', '%' . $input_home_search . '%')
+          ->addOrderBy('o.updatedAt')
+          ->getQuery();
+    }
+
+    $orchards = $query->getResult();
+
+    return $this->render('HomeBundle:Default:find_event.html.twig', array('events' => $events, 'type' => $type));
+  }
+
 
 }
