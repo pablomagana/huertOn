94702dc90e685e43dc7f7e4779667b7d2c6b2403
pablomagub <pablomagub@gmail.com>
2017-02-16 16:54:27 +0100

    link event user

--- src/EventBundle/Controller/ShowController.php
+++ src/EventBundle/Controller/ShowController.php
@@ -11,11 +11,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ShowController extends Controller
 {
-
+  // mostrar evento segun id en parametro
   public function profileAction($id_event)
   {
     $event=$this->getDoctrine()->getRepository('EventBundle:Event')->findOneById($id_event);
-    return $this->render('EventBundle:Default:profile.html.twig', array('event' => $event));
+    return $this->render('EventBundle:Show:profile.html.twig', array('event' => $event));
+  }
+
+  // añade un participante al evento
+  public function addUserToEventAction($id_event)
+  {
+    $user=$this->container->get("orchard_service")->getUser();
+    $event= $this->getDoctrine()->getRepository("EventBundle:Event")->findOneById($id_event);
+    $em = $this->getDoctrine()->getManager();
+    if (!$event) {
+      return new JsonResponse("ko");
+    }
+    $event->addUser($user);
+    $em->persist($event);
+    $em->flush();
   }
 
 }
